85140e24486e412c94d32b4d3abe9a6ff5d609e2
SzymonOlek <szymonole123@gmail.com>
2021-05-19 12:00:31 +0200

    data warehouse routing and controllers

--- routes/dataWarehouse.js
+++ routes/dataWarehouse.js
@@ -1,10 +1,30 @@
 'use strict';
 module.exports = function (app) {
-    var favouriteList = require('../controllers/favouriteListController');
-
-    app.route('/v1/datawarehouse')
-        .get(favouriteList.list_all_favourite_lists);
+    var dataWarehouse = require('../controllers//dataWarehouseController');
 
 
+    app.route('/v1/datawarehouse/likedDishedForAgeGroupe')
+        .get(dataWarehouse.list_mostly_liked_dishes_by_each_age_groupe);
 
+    app.route('/v1/datawarehouse/mostlyAddedToFavouriteGroupedByDifficulty')
+        .get(dataWarehouse.list_mostly_added_to_favourite_by_each_difficulty_level);
+
+    app.route('/v1/datawarehouse/ingredientsMostlyAddedToShoppingsList')
+        .get(dataWarehouse.list_mostly_added_to_shopping_list);
+
+    app.route('/v1/datawarehouse/MostCommentedRecipes')
+        .get(dataWarehouse.list_most_commented_recipes);
+
+    app.route('/v1/datawarehouse/MostSearchedRecipesBySeasons')
+        .get(dataWarehouse.list_most_added_to_favourite_by_seasons);
+
+    app.route('/v1/datawarehouse/MostlyAddedToFavourite')
+        .get(dataWarehouse.list_most_added_to_favourite);
+
+    app.route('/v1/datawarehouse/MostlyLikedThisWeek')
+        .get(dataWarehouse.list_most_liked_this_week);
+
+    app.route('/v1/datawarehouse/MostlyCommentedThisWeek')
+        .get(dataWarehouse.list_most_commented_this_week);
+    
 };
