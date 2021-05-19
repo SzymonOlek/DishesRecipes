d2885a19a731b132ffdd46428ca2991de9be8ff3
SzymonOlek <szymonole123@gmail.com>
2021-05-19 12:04:18 +0200

    data warehouse routing and controllers

--- controllers/dataWarehouseController.js
+++ controllers/dataWarehouseController.js
@@ -6,7 +6,35 @@ var mongoose = require('mongoose'),
 
 
 
+exports.list_mostly_liked_dishes_by_each_age_groupe = function(req, res) {
 
-    exports.temp_fnc = function(req, res) {
+};
 
-    };
+exports.list_mostly_added_to_favourite_by_each_difficulty_level = function(req, res) {
+
+};
+
+exports.list_mostly_added_to_shopping_list = function(req, res) {
+
+};
+
+exports.list_most_commented_recipes = function(req, res) {
+
+};
+
+exports.list_most_added_to_favourite_by_seasons = function(req, res) {
+
+};
+
+exports.list_most_added_to_favourite = function(req, res) {
+
+};
+
+exports.list_most_liked_this_week = function(req, res) {
+
+};
+
+exports.list_most_commented_this_week = function(req, res) {
+
+};
+   
